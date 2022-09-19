e7afea4481a37d26d4129d02e172a500d1688d68
NazarPashazade <pashazade.nazar@gmail.com>
2022-09-19 21:13:49 +0400

    create frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Chat } from "./pages/Chat";
+import { Register } from "./pages/Register";
+import { Login } from "./pages/Login";
 
 function App() {
   return (
-    <div className="App">
-     kkkkkkk
-    </div>
+    <BrowserRouter>
+      <Routes>
+      <Route path="/register" element={<Register/>}></Route>
+      <Route path="/login" element={<Login/>}></Route>
+      <Route path="/chat" element={<Chat/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/Chat.jsx
@@ -0,0 +1,4 @@
+export function Chat() {
+    return <div>Chat Page</div>;
+  }
+  
